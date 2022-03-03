d10370b3f5fbf238f6d0a5afe3b92ec11b5b7bf8
Badr Jlil <Badr.Jlil@outlook.com>
2022-03-03 19:19:04 +0100

    Update main.py

--- main.py
+++ main.py
@@ -147,12 +147,14 @@ async def join(ctx):
   """Make the bot join the channel."""
 
   await ctx.author.voice.channel.connect()
+  await ctx.send(f'Joined your voice channel')
 
 @client.command()
 async def leave(ctx):
   """Make the bot leave rhe channel."""
 
   await ctx.voice_client.disconnect()
+  await ctx.send(f'Leaving :wave:')
 
 @client.command()
 async def play(ctx, *, url):
@@ -163,11 +165,28 @@ async def play(ctx, *, url):
   if not ctx.voice_client.is_playing():
     await player.queue(url, search=True)
     song = await player.play()
-    await ctx.send(f'Playing ... ` {song.name} `')
+    await ctx.send(f'Playing ` {song.name} `')
   else:
     song = await player.queue(url, search=True)
     await ctx.send(f'` {song.name} ` queued')
 
+
+@client.command()
+async def queue(ctx):
+  player = music.get_player(guild_id=ctx.guild.id)
+  await ctx.send(f"{' | '.join([song.name for song in player.current_queue()])}")
+
+@client.command()
+async def pause(ctx):
+  player = music.get_player(guild_id=ctx.guild.id)
+  song = await player.pause()
+  await ctx.send(f'Paused `{song.name}`')
+
+@client.command()
+async def resume(ctx):
+  player = music.get_player(guild_id=ctx.guild.id)
+  song = await player.resume()
+  await ctx.send(f'Resumed `{song.name}`')
 
 
 @client.command()
